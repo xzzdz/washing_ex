795fc1e3a7fe68fa9111606831ff837b6415d32e
kittikorn <kttikon160@gmail.com>
2024-07-16 13:16:32 +0700

    bottom_navi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:washing_ex/screen/home_screen.dart';
 
+import 'screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,7 +8,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
